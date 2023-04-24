2f552ccc4db5bafa0a92486296ee4d02da8b24fc
Amir Shokri <amirsh.nll@gmail.com>
2023-04-24 23:57:39 +0330

    update: update coin type

--- coin/models.py
+++ coin/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class CoinTypes(models.Model):
     name = models.CharField(max_length=100)
     symbol = models.CharField(max_length=100)
+    price = models.FloatField(
+        default=0,
+    )
+    minimum_purchase = models.IntegerField(default=0, null=True, blank=True)
