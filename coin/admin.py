9703135ec365f69c1f389366cc556e4583c5c676
Amir Shokri <amirsh.nll@gmail.com>
2023-04-25 00:16:26 +0330

    update : create user balance after user register

--- coin/admin.py
+++ coin/admin.py
@@ -1,1 +1,4 @@
 from django.contrib import admin
+from .models import CoinTypes as CoinTypesModel
+
+admin.site.register(CoinTypesModel)
--- user/models.py
+++ user/models.py
@@ -1,7 +1,26 @@
 from django.db import models
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from balance.serializers import BalanceSerializers
+from django.conf import settings
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 
 class CustomUser(AbstractUser):
     is_deleted = models.BooleanField(default=False)
+
+    @receiver(post_save, sender=settings.AUTH_USER_MODEL)
+    def create_user(sender, instance, created=False, **kwargs):
+        if created:
+            # create user balance
+            serializer = BalanceSerializers(data={"user": instance})
+            if serializer.is_valid():
+                serializer.save()
+            else:
+                # roll back user registered
+                try:
+                    user_obj = CustomUser.objects.get(pk=instance)
+                    user_obj.delete()
+                except CustomUser.DoesNotExist:
+                    pass
