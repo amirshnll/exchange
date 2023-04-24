34b81836ba09b21f4d3b7124f2ec2325f033585a
Amir Shokri <amirsh.nll@gmail.com>
2023-04-24 22:59:45 +0330

    add : coin type model

--- coin/models.py
+++ coin/models.py
@@ -1,1 +1,6 @@
 from django.db import models
+
+
+class CoinTypes(models.Model):
+    name = models.CharField(max_length=100)
+    symbol = models.CharField(max_length=100)
--- coin/serializers.py
+++ coin/serializers.py
@@ -1,1 +1,8 @@
 from rest_framework import serializers
+from .models import CoinTypes as CoinTypesModel
+
+
+class CoinTypesSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = CoinTypesModel
+        fields = "__all__"
