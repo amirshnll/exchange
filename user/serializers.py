b906c7afa1d2f3e56055b4dfb2d96c3fadfcb319
Amir Shokri <amirsh.nll@gmail.com>
2023-04-25 00:32:57 +0330

    add : user serializer

--- user/serializers.py
+++ user/serializers.py
@@ -1,1 +1,7 @@
 from rest_framework import serializers
+from .models import CustomUser as CustomUserModel
+
+class CustomUserSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUserModel
+        fields = "__all__"
