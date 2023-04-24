62eed9711e764bbf73cc3e0f6cc1d01467c4c838
Amir Shokri <amirsh.nll@gmail.com>
2023-04-25 02:50:41 +0330

    update : rename amout field in order

--- order/models.py
+++ order/models.py
@@ -11,6 +11,6 @@ class OrderStatus(models.TextChoices):
 class Order(models.Model):
     user = models.ForeignKey(CustomUserModel, on_delete=models.CASCADE)
     coin = models.ForeignKey(CoinTypesModel, on_delete=models.CASCADE)
-    coin_amount = models.IntegerField()
+    coin_count = models.IntegerField()
     order_price = models.IntegerField()
     status = models.CharField(choices=OrderStatus.choices, max_length=10)
