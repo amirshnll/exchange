60e3497386d6513cf2052ed7ea78dbf51d8070a8
Amir Shokri <amirsh.nll@gmail.com>
2023-04-25 18:21:11 +0330

    update: change coin_is_exists

--- coin/defs.py
+++ coin/defs.py
@@ -2,8 +2,4 @@ from .models import CoinTypes as CoinTypesModel
 
 
 def coin_is_exists(coin_name):
-    try:
-        coin_obj = CoinTypesModel.objects.get(name=coin_name)
-        return True
-    except CoinTypesModel.DoesNotExist:
-        return False
+    return CoinTypesModel.objects.filter(name=coin_name).exists()
